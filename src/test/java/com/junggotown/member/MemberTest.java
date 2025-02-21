cc6343f6e788ade216e403d276d7ba823ef1553c
wlgus0409 <diffn0409@gmail.com>
2025-02-21 13:50:29 +0900

    2025.02.21 - 리드미 수정

--- src/test/java/com/junggotown/member/MemberTest.java
+++ src/test/java/com/junggotown/member/MemberTest.java
@@ -10,8 +10,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.TestPropertySource;
@@ -19,7 +17,6 @@ import org.springframework.test.context.TestPropertySource;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2, replace = AutoConfigureTestDatabase.Replace.NONE)
 @TestPropertySource("classpath:application-test.yml") //test용 properties 파일 설정
 @SpringBootTest
 @Transactional
